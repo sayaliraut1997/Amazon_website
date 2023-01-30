cacae6ac778a281c3c98f708bac813c081224978
sxr210701@darden.com <sxr210701@darden.com>
2023-01-30 19:56:16 +0530

    changes implemented

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,7 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { Link } from "react-router-dom";
 import { useStateValue } from './StateProvider';
 
+
   function Header() {
     const [{basket}, dispatch] = useStateValue();
   return (
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -68,5 +68,4 @@ function Home() {
    </div>
   );
 }
-
 export default Home;
